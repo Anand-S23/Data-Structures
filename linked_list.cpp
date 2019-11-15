c95e2b81447de955d3e19fc09929d36fc4f2156d
Anand-S23 <42609735+Anand-S23@users.noreply.github.com>
2019-11-15 13:55:34 -0500

    working code

--- linked_list.cpp
+++ linked_list.cpp
@@ -56,6 +56,8 @@ public:
             {
                 std::cout << ", " << next->value;
             }
+            
+            next = next->link;
         }
         std::cout << ")" << std::endl;
     }
