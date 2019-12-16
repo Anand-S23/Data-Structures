1e8471377db0c7e2b69995c24513894164ce981a
Anand-S23 <anandsroopsingh23@gmail.com>
2019-12-16 15:07:26 -0500

    bug fix

--- linked_list.c
+++ linked_list.c
@@ -25,31 +25,22 @@ void insert(LL list, int data, int pos)
     Node* next = list.head;
     Node* in_node = new_node(0, NULL);
 
-    if (pos > list.len + 1)
+    if (pos == 0)
     {
-        printf("The list is too short, cannot add node there.\n");
-    } 
+        in_node->link = list.head->link;
+        list.head->link = in_node;
+    }
     else
     {
-        if (pos == 0)
-        {
-            in_node->link = list.head->link;
-        list.head = in_node;
-        }
-        else
-        {
-            int i;
-                
-            for (i = 0; i < pos; i++)
+        for (i = 0; i < pos; i++)
         {
             next = next->link;
         }
         in_node->link = next->link;
         next = in_node;
-        }
-        
-        list.len++;
     }
+        
+    list.len++;
 }
 
 void print_list(LL list)
@@ -70,25 +61,6 @@ void main(void)
 {
     LL list = new_list();
     insert(list, 1, 0);
-    insert(list, 2, 0);
-    insert(list, 3, 0);
-    insert(list, 4, 0);
-    insert(list, 5, 0);
-    insert(list, 6, 0);
 
     print_list(list);
-
-    Node* next = list.head;
-    int i; 
-
-    for (i = 0; i < list.len; i++)
-    {
-        next = next->link;
-        printf("%d ", next->data);
-    }
-
-    printf("\n");
-
-    printf("The value of the head is %d\n", list.head->link->data);
-    printf("is this even working?");
 }
